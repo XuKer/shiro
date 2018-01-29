d59e17d11ac419e7df0f16fcc29b3b19a016b83e
XuKer <wxk1035@gmail.com>
2018-01-29 17:34:57 +0800

    jdbc、授权开始

--- shirotest1/src/main/java/com/xuke/shiro/chapter2/LoginTest.java
+++ shirotest1/src/main/java/com/xuke/shiro/chapter2/LoginTest.java
@@ -1,5 +1,6 @@
 package com.xuke.shiro.chapter2;
 
+import org.apache.log4j.net.SyslogAppender;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UsernamePasswordToken;
@@ -12,7 +13,7 @@ import org.junit.Test;
 import junit.framework.Assert;
 
 public class LoginTest {
-    @Test
+    
 	public void testHelloWorld(){
     	Factory<org.apache.shiro.mgt.SecurityManager> factory=
     			new IniSecurityManagerFactory("classpath:shiro-realm.ini");
@@ -30,4 +31,34 @@ public class LoginTest {
     	 Assert.assertEquals(true, subject.isAuthenticated());
     	 subject.logout();;
     }
+     
+    public void testJdbcRealms(){
+   	  
+    	 Factory<org.apache.shiro.mgt.SecurityManager> factory =
+    		
+                 new IniSecurityManagerFactory("classpath:shiro-jdbc-realm.ini");
+    	 SecurityManager securityManager = factory.getInstance();
+    	 SecurityUtils.setSecurityManager(securityManager);
+    	 Subject subject = SecurityUtils.getSubject();
+    	 UsernamePasswordToken token=new UsernamePasswordToken("zhang","123");
+    	 subject.login(token);
+    	 
+    	 Assert.assertEquals(true, subject.isAuthenticated());
+    	 subject.logout();
+     }
+    
+    @Test
+    public void testRealms(){
+   	  
+    	 Factory<org.apache.shiro.mgt.SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro-authenticator-all-success2.ini");
+    	 SecurityManager securityManager = factory.getInstance();
+    	 SecurityUtils.setSecurityManager(securityManager);
+    	 Subject subject = SecurityUtils.getSubject();
+    	 UsernamePasswordToken token=new UsernamePasswordToken("zhang","123");
+    	 subject.login(token);
+    	 
+    	 Assert.assertEquals(true, subject.isAuthenticated());
+    	 subject.logout();
+     }
+    
 }
